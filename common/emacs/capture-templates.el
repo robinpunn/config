fbdc02f980e0416ab65ca8788d278b7c6b01fbe2
pop <robin.punnoose@protonmail.com>
2025-09-08 18:42:30 -0500

    updated trading journal template to populate new trades as first heading

--- common/emacs/capture-templates.el
+++ common/emacs/capture-templates.el
@@ -17,20 +17,22 @@
                                (line-beginning-position)
                              (point-max))))
            (clean-ticker (string-trim ticker))
-           (found nil))
+           (found nil)
+           (ticker-start nil))
       ;; Loop through ** headings after * Watch
       (while (and (not found)
                   (re-search-forward "^\\*\\* \\(.*\\)$" search-limit t))
         (let ((heading (string-trim (match-string 1))))
           (when (string= heading clean-ticker)
-            (setq found t))))
+            (setq found t)
+            (setq ticker-start (line-beginning-position)))))
       (if found
-          ;; Go to end of this heading's subtree
+          ;; Go to the line after the ticker heading (top of its content)
           (progn
-            (goto-char (match-beginning 0)) ; start of that heading
-            (outline-end-of-subtree)
+            (goto-char ticker-start)
+            (forward-line 1)
             (point))
-        ;; Create new
+        ;; Create new ticker heading
         (goto-char search-limit)
         (unless (bolp) (insert "\n"))
         (insert "** " clean-ticker "\n")
@@ -41,33 +43,30 @@
   (interactive)
   (if (not (org-at-table-p))
       (message "Not in a table!")
-    (let* ((ticker (org-table-get-field 1))
-           (raw-trade-type (org-table-get-field 2)) ; includes quotes e.g. "\"call\""
-           ;; First remove quotes, then trim whitespace
+    (let* ((vals (my/extract-values-from-current-calculations-row))
+           (ticker (cdr (assoc 'ticker vals)))
+           (raw-trade-type (cdr (assoc 'type vals)))
+           ;; Clean up the type field - remove quotes and trim whitespace
            (trade-type (string-trim (replace-regexp-in-string "\"" "" raw-trade-type)))
+           (kijun (cdr (assoc 'kijun vals)))
+           (delta (cdr (assoc 'delta vals)))
+           (atr (cdr (assoc 'atr vals)))
+           (pba (cdr (assoc 'pba vals)))
            (confirm (y-or-n-p (format "Create %s trade template for %s? " trade-type ticker))))
       (when confirm
-        (let* ((vals (my/extract-values-from-current-calculations-row))
-               (kijun (cdr (assoc 'kijun vals)))
-               (delta (cdr (assoc 'delta vals)))
-               (atr (cdr (assoc 'atr vals)))
-               (pba (cdr (assoc 'pba vals)))
-               ;; Debug: let's see what we're comparing
-               (_ (message "Debug: trade-type is '%s'" trade-type))
-               (is-options (or (string= trade-type "call") (string= trade-type "put")))
-               (_ (message "Debug: is-options is %s" is-options))
+        (let* ((is-options (or (string= trade-type "call") (string= trade-type "put")))
                (trade-text
                 (if is-options
                     (format
-                     "*** %s\n**** opening indicators\n***** daily\n - kijun\n   - value: %s\n   - direction:\n - pbar: %s\n - stoch:\n   - %%k:\n   - %%d:\n - cmf:\n   - value:\n   - direction:\n - rsi:\n   - value:\n   - direction:\n - atr: %s\n - hv:\n\n***** chain\n - delta: %s\n - gamma:\n - vega:\n - theta:\n - ivr:\n - ivx:\n\n**** open\n***** fill\n - date:\n - strike:\n - exp:\n - quantity:\n - price:\n**** close\n***** take profit 1\n - date:\n - strike:\n - exp:\n - quantity:\n - price:\n***** trade close\n - date:\n - strike:\n - exp:\n - quantity\n - price:\n\n**** lessons\n"
-                     (format-time-string "%m/%d/%y") kijun pba atr delta)
+                     "*** %s\n - type: %s\n**** opening indicators\n***** daily\n - kijun\n   - value: %s\n   - direction:\n - pbar: %s\n - stoch:\n   - %%k:\n   - %%d:\n - cmf:\n   - value:\n   - direction:\n - rsi:\n   - value:\n   - direction:\n - atr: %s\n - hv:\n\n***** chain\n - delta: %s\n - gamma:\n - vega:\n - theta:\n - ivr:\n - ivx:\n\n**** open\n***** fill\n - date:\n - strike:\n - exp:\n - quantity:\n - price:\n**** close\n***** take profit 1\n - date:\n - strike:\n - exp:\n - quantity:\n - price:\n***** trade close\n - date:\n - strike:\n - exp:\n - quantity\n - price:\n\n**** lessons\n"
+                     (format-time-string "%m/%d/%y") trade-type kijun pba atr delta)
                   (format
-                   "*** %s\n**** opening indicators\n***** daily\n - kijun\n   - value: %s\n   - direction:\n - pbar: %s\n - stoch:\n   - %%k:\n   - %%d:\n - cmf:\n   - value:\n   - direction:\n - rsi:\n   - value:\n   - direction:\n - atr: %s\n - hv:\n\n**** open\n***** fill\n - date:\n - quantity:\n - price:\n**** close\n***** take profit 1\n - date:\n - quantity:\n - price:\n***** trade close\n - date:\n - quantity\n - price:\n\n**** lessons\n"
-                   (format-time-string "%m/%d/%y") kijun pba atr))))
+                   "*** %s\n - type: %s\n**** opening indicators\n***** daily\n - kijun\n   - value: %s\n   - direction:\n - pbar: %s\n - stoch:\n   - %%k:\n   - %%d:\n - cmf:\n   - value:\n   - direction:\n - rsi:\n   - value:\n   - direction:\n - atr: %s\n - hv:\n\n**** open\n***** fill\n - date:\n - quantity:\n - price:\n**** close\n***** take profit 1\n - date:\n - quantity:\n - price:\n***** trade close\n - date:\n - quantity\n - price:\n\n**** lessons\n"
+                   (format-time-string "%m/%d/%y") trade-type kijun pba atr))))
           (with-current-buffer (find-file-noselect "~/Documents/Practice/trades.org")
             (let ((pos (my/find-or-create-ticker-heading ticker)))
               (goto-char pos)
               (unless (bolp) (insert "\n"))
               (insert trade-text)
               (save-buffer)
-              (message "%s trade template created for %s" trade-type ticker))))))))
+              (message "%s trade template created for %s" trade-type ticker)))))))))
