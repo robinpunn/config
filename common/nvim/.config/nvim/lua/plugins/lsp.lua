fc33ea7ca32b8a9d410912c1dbf6773d5d3d0519
pop <robin.punnoose@protonmail.com>
2025-12-22 22:58:53 -0600

    updated nvim-lspconfig

--- common/nvim/.config/nvim/lua/plugins/lsp.lua
+++ common/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    local lspconfig = vim.lsp.config()
     local on_attach = function(client, bufnr)
       local buf_map = function(mode, lhs, rhs, desc)
         vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
@@ -10,35 +9,26 @@ return {
       buf_map("n", "<leader>gd", vim.lsp.buf.definition, "Go to definition")
       buf_map("n", "K", vim.lsp.buf.hover, "Show hover")
     end
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    
-    -- Python LSP
-    lspconfig.pylsp.setup({
-      on_attach = on_attach,
-      capabilities=capabilities
-    })
 
-    -- TypeScript / JavaScript LSP (typescript-language-server)
-    lspconfig.ts_ls.setup({
-      on_attach = on_attach,
-      capabilities=capabilities
-    })
-    -- Go LSP (gopls)
-    lspconfig.gopls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    -- Go linting (golangci-lint) 
-    lspconfig.golangci_lint_ls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
-    -- HTML LSP
-    lspconfig.html.setup({
-      on_attach = on_attach,
-      capabilities=capabilities
-    })
+    -- Python
+    vim.lsp.config("pylsp", { on_attach = on_attach, capabilities = capabilities })
+
+    -- TS / JS
+    vim.lsp.config("ts_ls", { on_attach = on_attach, capabilities = capabilities })
+
+    -- Go
+    vim.lsp.config("gopls", { on_attach = on_attach, capabilities = capabilities })
+
+    -- Go lint
+    vim.lsp.config("golangci_lint_ls", { on_attach = on_attach, capabilities = capabilities })
+
+    -- HTML
+    vim.lsp.config("html", { on_attach = on_attach, capabilities = capabilities })
+
+    -- Enable servers
+    vim.lsp.enable({ "pylsp", "ts_ls", "gopls", "golangci_lint_ls", "html" })
   end,
 }
 
