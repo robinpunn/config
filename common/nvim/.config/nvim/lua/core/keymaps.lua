fa8dbf537d40f9a372f0bb8a7f833270be86f456
pop <robin.punnoose@protonmail.com>
2025-12-23 19:32:50 -0600

    added :Telescope grep_string keybind, added quickfix keybinds, removed noremap (not needed with vim.keymap.set

--- common/nvim/.config/nvim/lua/core/keymaps.lua
+++ common/nvim/.config/nvim/lua/core/keymaps.lua
@@ -9,17 +9,24 @@ vim.diagnostic.config({
 })
 
 -- netrw
-vim.keymap.set("n", "<C-b>", ":Lex<CR>", { noremap = true, silent = true, desc = "Toggle netrw Lex" })
-vim.keymap.set("n", "<leader>b", ":Ex<CR>", { noremap = true, silent = true, desc = "Open netrw Ex" })
-vim.keymap.set("n", "<leader>B", ":Vex<CR>", { noremap = true, silent = true, desc = "Open netrw Vex" })
+vim.keymap.set("n", "<C-b>", "<cmd>Lex<CR>", { silent = true, desc = "Toggle netrw Lex" })
+vim.keymap.set("n", "<leader>b", "<cmd>Ex<CR>", { silent = true, desc = "Open netrw Ex" })
+vim.keymap.set("n", "<leader>B", "<cmd>Vex<CR>", { silent = true, desc = "Open netrw Vex" })
 
 -- buffer fast travel
-vim.keymap.set("n", "[b", ":bp<CR>", { noremap = true, silent = true, desc = "Go to previous buffer" })
-vim.keymap.set("n", "]b", ":bn<CR>", { noremap = true, silent = true, desc = "Go to next buffer" })
+vim.keymap.set("n", "[b", "<cmd>bprevious<CR>", { silent = true, desc = "Go to previous buffer" })
+vim.keymap.set("n", "]b", "<cmd>bnext<CR>", { silent = true, desc = "Go to next buffer" })
 
 -- tabs
-vim.keymap.set("n", "<leader>gT", ":tabmove -1<CR>", { noremap = true , desc = "Move tab left" })
-vim.keymap.set("n", "<leader>gt", ":tabmove +1<CR>", { noremap = true, desc= "Move tab right" })
+vim.keymap.set("n", "<leader>gT", "<cmd>tabmove -1<CR>", { desc = "Move tab left" })
+vim.keymap.set("n", "<leader>gt", "<cmd>tabmove +1<CR>", { desc = "Move tab right" })
+
+-- quickfix
+vim.keymap.set("n", "<leader>qo", "<cmd>copen<CR>",  { desc = "Quickfix open" })
+vim.keymap.set("n", "<leader>qc", "<cmd>cclose<CR>", { desc = "Quickfix close" })
+vim.keymap.set("n", "<leader>qn", "<cmd>cnext<CR>",  { desc = "Quickfix next" })
+vim.keymap.set("n", "<leader>qp", "<cmd>cprev<CR>",  { desc = "Quickfix prev" })
+vim.keymap.set("n", "<leader>qz", "<cmd>cgetexpr []<CR>",  { desc = "Quickfix clear" })
 
 -- spell check
-vim.keymap.set("n", "<leader>S", ":set spell!<CR>", {noremap = true, desc = "Toggle spell check"})
+vim.keymap.set("n", "<leader>S", ":set spell!<CR>", { desc = "Toggle spell check" })
--- common/nvim/.config/nvim/lua/plugins/telescope.lua
+++ common/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -26,6 +26,7 @@ return {
   keys = {
     { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Telescope find files' },
     { '<leader>fg', function() require('telescope.builtin').live_grep() end, desc = 'Telescope live grep' },
+    { '<leader>fs', function() require('telescope.builtin').grep_string() end, desc = 'Telescope grep string under cursor' },
     { '<leader>fb', function() require('telescope.builtin').buffers() end, desc = 'Telescope buffers' },
     { '<leader>fh', function() require('telescope.builtin').help_tags() end, desc = 'Telescope help tags' },
     { '<leader>fo', function() require('telescope.builtin').oldfiles() end, desc = 'Find old files' },
