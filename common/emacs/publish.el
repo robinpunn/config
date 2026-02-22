faf3d91d518c3290eb9cf1f7605847db32b5a9ea
robinpunn <robin.punnoose@protonmail.com>
2026-02-22 16:55:32 -0600

    updated publish.el with preview function which should open a preview of html blog from notes or blog dir

--- common/emacs/publish.el
+++ common/emacs/publish.el
@@ -5,11 +5,10 @@
 (defvar my/blog-org-dir (expand-file-name "org/" my/blog-base-dir))
 (defvar my/blog-public-dir (expand-file-name "public/" my/blog-base-dir))
 (defvar my/blog-static-dir (expand-file-name "static/" my/blog-base-dir))
-(defvar my/blog-pi-path "pi@192.168.0.33:/home/pi/blog/")
+(defvar my/blog-pi-path "pi@192.168.0.33:/home/pi/Blog/")
 
 (setq org-publish-project-alist
-      `(
-	("blog-org"
+      `(("blog-org"
 	 :base-directory ,my/blog-org-dir
 	 :base-extension "org"
 	 :publishing-directory ,my/blog-public-dir
@@ -17,15 +16,15 @@
 	 :with-tags t
 	 :recursive t
 
-	 :author 'Robin Punnoose'
+	 :author "Robin Punnoose"
 
-	 :auto-sitemap 1
+	 :auto-sitemap t
 	 :sitemap-filename "index.org"
 	 :sitemap-title "Blog Posts"
 	 :sitemap-sort-files anti-chronologically
 
 	 :section-numbers nil
-	 :with-toc nil
+	 :with-toc t
 	 :html-head "<link rel=\"stylesheet\" 
 		href=\"/css/style.css\" 
 	 	type=\"text/css\" />"
@@ -52,9 +51,53 @@
     (copy-file src dest t)
     (message "Copied to blog: %s" (file-name-nondirectory src))))
 
-(defun my/publish-post()
+(defun my/blog-org-path ()
+  (let ((filename (file-name-nondirectory (buffer-file-name))))
+    (expand-file-name filename my/blog-org-dir)))
+
+(defun my/blog-html-path ()
+  (let* ((filename (file-name-nondirectory (buffer-file-name)))
+         (html-name (concat (file-name-sans-extension filename) ".html")))
+    (expand-file-name html-name my/blog-public-dir)))
+
+(defun my/open-blog-copy ()
   (interactive)
-  (org-publish-current-file))
+  (find-file (my/blog-org-path)))
+
+(defun my/publish-current-blog-file ()
+  (interactive)
+  (org-publish-current-file t))
+
+(defun my/open-blog-html ()
+  (interactive)
+  (let ((html-path (my/blog-html-path)))
+    (unless (file-exists-p html-path)
+      (user-error "Published HTML does not exist. Publish first."))
+    (browse-url-of-file html-path)))
+
+(defun my/preview-post-from-notes ()
+  (interactive)
+  (my/copy-from-notes)
+  (my/open-blog-copy)
+  (my/publish-current-blog-file)
+  (my/open-blog-html))
+
+(defun my/preview-post-from-source ()
+  (interactive)
+  (my/publish-current-blog-file)
+  (my/open-blog-html))
+
+(defun my/in-blog-source-p ()
+  (and (buffer-file-name)
+       (file-in-directory-p
+        (buffer-file-name)
+        my/blog-org-dir)))
+
+(defun my/preview-post ()
+  (interactive)
+  (if (my/in-blog-source-p)
+      (my/preview-post-from-source)
+    (my/preview-post-from-notes)))
 
 (defun my/publish-blog()
   (interactive)
@@ -62,5 +105,5 @@
 
 (defun my/publish-and-deploy()
   (interactive)
-  (org-publish-project "blog")
+  (my/publish-blog)
   (shell-command "rsync -avz %s %s" my/blog-public-dir my/blog-pi-path))
