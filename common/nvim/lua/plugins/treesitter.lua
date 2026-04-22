7d2d6718d2a04fb1a81d9efbbcba65a85c27a94f
arch <robin.punnoose@protonmail.com>
2026-04-21 19:17:53 -0500

    updated neovim treesitter config

--- common/nvim/lua/plugins/treesitter.lua
+++ common/nvim/lua/plugins/treesitter.lua
@@ -2,19 +2,19 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require('nvim-treesitter.configs').setup {
+    require('nvim-treesitter').setup {
       ensure_installed = {
         "python",
         "html",
         "javascript",
         "typescript",
         "tsx",
-	"go",
-	"solidity",
-	"rust",
-	"c",
-	"asm",
-	"commonlisp",
+	      "go",
+	      "solidity",
+	      "rust",
+	      "c",
+	      "asm",
+	      "commonlisp",
       },
       highlight = { enable = true },
       indent = { enable = true },
